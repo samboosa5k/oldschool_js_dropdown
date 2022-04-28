3c682f36e874a8432242323375da25b591c64a31
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2022-04-28 22:09:27 +0200

    oldschool html javascript example

---
+++ dropdownScript.js
@@ -0,0 +1,40 @@
+//THIS IS WHATEVER YOUR ACCOUNT ID IS
+const ACCOUNT_ID = "abc123";
+
+//THIS IS THE FETCH RESPONSE
+const fetchResponse = [
+  { id: "option1", name: "option 1", value: 1 },
+  { id: "option2", name: "option 2", value: 2 },
+  { id: "option3", name: "option 3", value: null },
+];
+
+//THIS IS THE CALLBACK FUNCTION
+const callbackFunction = (value, accountID) => {
+  if (typeof value === "number") {
+    console.log(`I am addind ${value} to yout bank account ${accountID}`);
+  } else {
+    console.log("The provided value is not a number");
+  }
+};
+
+/**
+ * 
+ * @param {callbackFunction} callback - The callback function to be called when the <option> is selected
+ * @param {string} parentID - the id of the <selector> wrapper
+ * @param {string} accountID - the id of the account to be updated
+ * @returns 
+ */
+const optionList = (callback, parentID, accountID) =>
+  fetchResponse.map((sqlObject, i) => {
+    const dropdownOption = document.createElement("option");
+    const dropdownLabel = document.createTextNode(sqlObject.name);
+    dropdownOption.setAttribute("id", sqlObject.id);
+
+    // THIS IS WHERE YOU ADD THE CLICK LISTENER TO RETURN THE CALLBACK & PARAMETERS
+    dropdownOption.addEventListener("click", () =>
+      callback(sqlObject.value, accountID)
+    );
+    dropdownOption.appendChild(dropdownLabel);
+
+    document.querySelector(parentID).appendChild(dropdownOption);
+  });
